9991ffb1a9b0198046438f7162ea92a1278cdb9d
dobam <ckehqja@gmail.com>
2024-05-02 19:13:14 +0900

    dd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        System.out.println('d');
+        DBStorage dbStorage = new DBStorage();
         //test chadd
     }
 }
