94ae5a0cbc4991fce4ca816ab679b9356719f6d7
Chanwon-Seo <scwonn60@gmail.com>
2024-05-02 23:36:48 +0900

    fix : 등록된 회차 버그 수정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,10 +16,10 @@ public class Main {
         //수강생 점수 등록
         //수강생 고유번호 및 과목 고유번호 입력 받은 후
         //TODO Scanner로 입력
-        Integer studentId = 0;
-        Integer subjectId = 0;
+        Integer studentId = 2023001;
+        Integer subjectId = 1;
         Long round = 0L;
-        Long scoreNum = 0L;
+        Long scoreNum = 100L;
         studentScoreSaveValidation(studentId, subjectId, round, scoreNum);
         Score score = new ScoreService().scoreCreateV1(subjectId, studentId, round, scoreNum);
         dbStorage.saveScoreList(score);
@@ -46,15 +46,17 @@ public class Main {
                 System.out.println("존재하지 않는 과목입니다.");
             }
         }
-        List<Score> scoreList = dbStorage.getScoreList();
-        if (scoreList.size() <= roundNum) {
-            System.out.println("이미 등록된 회차입니다.");
-        }
         if (roundNum < 1 || 10 < roundNum) {
-            System.out.println("회차 범위는 1~10까지 입니다.");
+            System.out.println("회차 범위는 1 ~ 10까지 입니다.");
         }
         if (scoreNum < 0 || 100 < scoreNum) {
-            System.out.println("점수 범위는 0~ 100까지 입니다.");
+            System.out.println("점수 범위는 0 ~ 100까지 입니다.");
         }
+
+        List<Score> scoreList = dbStorage.getScoreList();
+        if (scoreList.size() >= roundNum) {
+            System.out.println("이미 등록된 회차입니다.");
+        }
+
     }
 }
