8f53412e27bb8b93d9d7cab3d4de869fbc8b5618
Chanwon-Seo <scwonn60@gmail.com>
2024-05-03 04:53:30 +0900

    fix : 회차 및 점수 타입 변경

--- src/main/java/org/example/domain/Score.java
+++ src/main/java/org/example/domain/Score.java
@@ -9,14 +9,14 @@ public class Score {
     private Integer subjectId; //과목 고유 번호
     private Integer studentId; //수강생 고유번호
 
-    private Map<Long, Long> scoreId; //회차 점수리스트
+    private Map<Integer, Integer> scoreId; //회차 점수리스트
 
     private Character level; //점수 레벨
 
-    public Score(Integer subjectId, Integer studentId, Map<Long, Long> scoreId, Character level) {
+    public Score(Integer subjectId, Integer studentId, Map<Integer, Integer> scoreId, Character level) {
         this.subjectId = subjectId;
         this.studentId = studentId;
-        this.scoreId = scoreId; //LinkedHashMap
+        this.scoreId = scoreId;
         this.level = level;
     }
 }
