d76ee24bdafed2bb142f4bc9714aee889c65e64b
dobam <ckehqja@gmail.com>
2024-05-02 19:16:12 +0900

    g

--- src/main/java/domain/Student.java
+++ src/main/java/domain/Student.java
@@ -5,7 +5,7 @@ import java.util.Set;
 public class Student {
     private Integer studentIdInteger; //수강생 고유 번호
 
-
+//dd
     private String birthDay; //생년월일
 
     private Set<Long> subjectId; //과목 고유 번호
