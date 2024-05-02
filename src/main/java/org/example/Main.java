b2325693b037cc49791d3e26fdc972d931de7382
SeongKyun <yooss135@naver.com>
2024-05-02 19:15:22 +0900

    main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+
+        System.out.printf("Hello and welcome!!@!@");
     }
 }
