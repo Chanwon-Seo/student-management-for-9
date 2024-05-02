f5d9b877d179a64d2bcec45b57a1ca2cdd453a39
dobam <ckehqja@gmail.com>
2024-05-02 20:02:16 +0900

    feat 학생리스트 메서드, 학생 상세 메서드 추가

--- src/main/java/org/example/service/StudentService.java
+++ src/main/java/org/example/service/StudentService.java
@@ -4,7 +4,28 @@ package org.example.service;
 import org.example.DBStorage;
 import org.example.domain.Student;
 
+import java.util.List;
+
 public class StudentService {
-    Student student = new Student();
-    DBStorage dbStorage = new DBStorage();
+    public void getStudentList(DBStorage dbStorage) {
+        List<Student> studentList = dbStorage.getStudentList();
+        for (Student student : studentList) {
+            System.out.println("id / name");
+            System.out.println(student.getStudentIdInteger() + " : " + student.getStudentName());
+        }
+    }
+
+    public void getStudentDetail(Student student) {
+        System.out.println("id : " + student.getStudentIdInteger());
+        System.out.println("이름 : " + student.getStudentName());
+        System.out.println("생년월일 : " + student.getBirthDay());
+        System.out.println("상태 : " + student.getStudentState());
+        /*
+        * 추후 학생 아이디 일칳라는 과목 반목문
+        * */
+        for (Long subjectId : student.getSubjectId()) {
+            System.out.println("과목 : " + subjectId);
+        }
+    }
+
 }
