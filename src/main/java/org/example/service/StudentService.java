4888477b9b03b5bb17977ac34a711e48726aef10
SeongKyun <yooss135@naver.com>
2024-05-03 11:10:01 +0900

    feat: 수강생 생성기능 추가

--- src/main/java/org/example/service/StudentService.java
+++ src/main/java/org/example/service/StudentService.java
@@ -10,9 +10,13 @@ import org.example.domain.Subject;
 import java.util.*;
 
 public class StudentService {
+    static final int MIN_REQUIRED_SUBJECTS = 3;
+    static final int MIN_ELECTIVE_SUBJECTS = 2;
     List<Subject> sub = DBStorage.getSubjectList();
     Set<Integer> subjectId = new HashSet<>();
     Scanner sc = new Scanner(System.in);
+    int rSub = 0;
+    int eSub = 0;
 
     //수강생 등록, 조회 화면
     public void displayStudentView () {
@@ -26,6 +30,7 @@ public class StudentService {
             switch (choice) {
                 case 1 -> createStudent();
                 //case 2 ->
+                case 3 -> {return;}
                 default -> System.out.println("잘못 입력하셨습니다.");
             }
         }
@@ -33,8 +38,8 @@ public class StudentService {
 
     //수강자 생성
     public void createStudent () {
-        String name = inputStirng("수강생 이름 입력: ");
-        String birth = inputStirng("수강생 생년월일 입력: ");
+        String name = inputString("수강생 이름 입력: ");
+        String birth = inputString("수강생 생년월일 입력: ");
 
         sub.forEach(subject -> {
            System.out.println("고유ID: " + subject.getSubjectId() + ", 제목: " + subject.getSubjectName() + ", 과목: " + subject.getSubjectType());
@@ -47,13 +52,29 @@ public class StudentService {
             subjectId.add(subId);
         }
 
-        System.out.println("수강자가 생성되었습니다.");
-        Student st = new Student(++Main.uNumber, name, birth, subjectId);
-        DBStorage.addStudentList(st);
+        if(rSub>=MIN_REQUIRED_SUBJECTS && eSub>=MIN_ELECTIVE_SUBJECTS) {
+            System.out.println("수강자가 생성되었습니다.");
+            Student st = new Student(++Main.uNumber, name, birth, subjectId);
+            DBStorage.addStudentList(st);
+            rSub=0;
+            eSub=0;
+        }
+        else if(rSub<MIN_REQUIRED_SUBJECTS && eSub<MIN_ELECTIVE_SUBJECTS){
+            System.out.println("필수과목이 " + (MIN_REQUIRED_SUBJECTS-rSub) + "과목, 선택과목이 " + (MIN_ELECTIVE_SUBJECTS-eSub) + "선택과목이 부족해 수강생이 등록되지 않습니다.");
+        }
+        else if(rSub<MIN_REQUIRED_SUBJECTS){
+            System.out.println("필수과목이 " + (MIN_REQUIRED_SUBJECTS-rSub) + "과목 부족해 수강생이 등록되지 않습니다.");
+        }
+        else {
+            System.out.println("선택과목이 " + (MIN_ELECTIVE_SUBJECTS-eSub) + "과목 부족해 수강생이 등록되지 않습니다.");
+
+        }
+
+
     }
 
     //String 값 입력
-    public String inputStirng(String m){
+    public String inputString(String m){
         System.out.print(m);
         return sc.nextLine();
     }
@@ -68,12 +89,18 @@ public class StudentService {
             for(Subject si:sub){
                 if(si.getSubjectName().equals(s)){
                     System.out.println("과목 추가 완료");
+
+                    if(si.getSubjectType().equals("SUBJECT_TYPE_MANDATORY")){
+                        rSub++;
+                    }
+                    else{
+                        eSub++;
+                    }
+
                     return si.getSubjectId();
                 }
             }
             System.out.println("일치하는 과목이 없습니다. 다시 입력해주세요.");
         }
     }
-
-
 }
