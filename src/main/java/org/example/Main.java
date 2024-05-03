b569661b6e55806b5c4aec005e589329f774350f
SeongKyun <yooss135@naver.com>
2024-05-03 10:37:13 +0900

    feat: 수강생 생ì성 기ë¥ 추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,19 @@
 package org.example;
 
+import org.example.service.StudentService;
+import org.example.domain.Score;
+import org.example.domain.Student;
+import org.example.domain.Subject;
 
 public class Main {
+
+    public static Integer uNumber = 2024000;
+
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!!@!@");
+        StudentService st = new StudentService();
+        while(true) {
+            st.displayStudentView();
+        }
     }
 }
--- src/main/java/org/example/service/StudentService.java
+++ src/main/java/org/example/service/StudentService.java
@@ -1,10 +1,79 @@
 package org.example.service;
 
 
+import lombok.Getter;
 import org.example.DBStorage;
+import org.example.Main;
 import org.example.domain.Student;
+import org.example.domain.Subject;
+
+import java.util.*;
 
 public class StudentService {
-    Student student = new Student();
-    DBStorage dbStorage = new DBStorage();
+    List<Subject> sub = DBStorage.getSubjectList();
+    Set<Integer> subjectId = new HashSet<>();
+    Scanner sc = new Scanner(System.in);
+
+    //수강생 등록, 조회 화면
+    public void displayStudentView () {
+        while(true) {
+            System.out.println("==================================");
+            System.out.println("1. 수강생 등록");
+            System.out.println("2. 수강생 목록 조회");
+            System.out.println("3. 메인 화면 이동");
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch (choice) {
+                case 1 -> createStudent();
+                //case 2 ->
+                default -> System.out.println("잘못 입력하셨습니다.");
+            }
+        }
+    }
+
+    //수강자 생성
+    public void createStudent () {
+        String name = inputStirng("수강생 이름 입력: ");
+        String birth = inputStirng("수강생 생년월일 입력: ");
+
+        sub.forEach(subject -> {
+           System.out.println("고유ID: " + subject.getSubjectId() + ", 제목: " + subject.getSubjectName() + ", 과목: " + subject.getSubjectType());
+        });
+
+        System.out.println("\n수강할 과목의 제목을 입력해주세요. (종료 exit)");
+
+        Integer subId;
+        while((subId=addSubject()) != 0){
+            subjectId.add(subId);
+        }
+
+        System.out.println("수강자가 생성되었습니다.");
+        Student st = new Student(++Main.uNumber, name, birth, subjectId);
+        DBStorage.addStudentList(st);
+    }
+
+    //String 값 입력
+    public String inputStirng(String m){
+        System.out.print(m);
+        return sc.nextLine();
+    }
+
+    //수강 과목 추가
+    public Integer addSubject(){
+        while(true){
+            String s = sc.nextLine();
+            if("exit".equals(s)){
+                return 0;
+            }
+            for(Subject si:sub){
+                if(si.getSubjectName().equals(s)){
+                    System.out.println("과목 추가 완료");
+                    return si.getSubjectId();
+                }
+            }
+            System.out.println("일치하는 과목이 없습니다. 다시 입력해주세요.");
+        }
+    }
+
+
 }
