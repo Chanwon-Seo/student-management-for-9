3f4dbf5cde1f966fb44ebab3ce894bf06629ff50
haze0827 <167048997+haze0827@users.noreply.github.com>
2024-05-03 11:59:06 +0900

    update. Menu

--- src/main/java/org/example/Menu.java
+++ src/main/java/org/example/Menu.java
@@ -1,4 +1,82 @@
 package org.example;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Scanner;
+
 public class Menu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Student[] students = new Student[100]; // 100명정도 배열생성
+        int studentCount = 0;
+
+        while (true) {
+            System.out.println("1. 수강생 관리");
+            System.out.println("2. 수강생 점수관리");
+            System.out.println("3. 프로그램 종료(exit)");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // 개행 문자 제거
+
+            switch (choice) {
+                case 1: // 수강생 관리 이동
+                    manageStudents(students, studentCount, scanner);
+                    break;
+                case 2: // 수강생 점수관리 이동
+                    manageScores(students, studentCount, scanner);
+                    break;
+                case 3: // 끝
+                    System.out.println("프로그램 종료...");
+                    return;
+                default:
+                    System.out.println("다시 입력바랍니다.");
+            }
+        }
+    }
+
+
+
+
+
+
+
+    static class Student {
+        @Setter
+        @Getter
+        private String studentName; // 학생이름
+        private final String studentNumber; // 학생 고유번호
+        private int java1; // 필수과목1
+        private int java2; // 선택과목2
+        private int java3; // 선택과목3
+
+
+
+
+        public Student(String studentName, String studentNumber, int java1, int java2, int java3) {
+            this.studentName = studentName;
+            this.studentNumber = studentNumber;
+            this.java1 = java1;
+            this.java2 = java2;
+            this.java3 = java3;
+        }
+
+
+        public void printInfo() {
+            System.out.println("학생이름: " + studentName);
+            System.out.println("고유번호: " + studentNumber);
+            System.out.println("필수과목: " + java1);
+            System.out.println("선택과목: " + java2);
+            System.out.println("선택과목: " + java3);
+        }
+    }
+
+
+    // 수강생 관리 메뉴 실행
+    private static void manageStudents(Student[] students, int studentCount, Scanner scanner) {
+    }
+    // 점수 관리 메뉴 실행
+    private static void manageScores(Student[] students, int studentCount, Scanner scanner) {
+
+    }
 }
