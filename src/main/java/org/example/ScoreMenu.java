764f1b9b19ec59984b97045ba38a2e384dfc3f7c
Chanwon-Seo <scwonn60@gmail.com>
2024-05-07 06:41:28 +0900

    fix : 수강생 점수 등록 v2

--- src/main/java/org/example/ScoreMenu.java
+++ src/main/java/org/example/ScoreMenu.java
@@ -1,27 +1,43 @@
 package org.example;
 
-import lombok.RequiredArgsConstructor;
+import org.example.domain.Subject;
 import org.example.parser.Parser;
 import org.example.service.ScoreService;
 
 import static org.example.Menu.dbStorage;
 import static org.example.Menu.sc;
 
-@RequiredArgsConstructor
 public class ScoreMenu {
     public void displayScoreView() {
+
         while (true) {
             System.out.println("1. 수강생 점수 등록");
             System.out.println("2. 수강생 점수 조회");
             System.out.println("3. 수강생 과목별 회차 점수 수정");
             System.out.println("4. 메인으로 이동");
-            int i = Integer.parseInt(sc.nextLine());
-            switch (i) {
-                case 1:
-                    new Parser().scoreCreate(1, 1, 1, 1);
-                    dbStorage.saveScoreList(new ScoreService().scoreCreateV1(1, 1, 1, 1));
-                    break;
+            try {
+                switch (Integer.parseInt(sc.nextLine())) {
+                    case 1:
+                        System.out.print("과목 고유번호 입력 : ");
+                        int subjectIdInput = Integer.parseInt(sc.nextLine());
+                        System.out.print("학생 고유번호 입력 : ");
+                        int studentIdInput = Integer.parseInt(sc.nextLine());
+                        System.out.print("회차 번호 입력 : ");
+                        int roundInput = Integer.parseInt(sc.nextLine());
+                        System.out.print("학생 점수 입력 : ");
+                        int scoreInput = Integer.parseInt(sc.nextLine());
+                        Subject findSubjectData = new Parser().scoreCreate(subjectIdInput, studentIdInput, roundInput, scoreInput);
+                        dbStorage.saveScoreList(new ScoreService().scoreCreateV1(findSubjectData, studentIdInput, roundInput, scoreInput));
+                        break;
+                    case 4:
+                        return;
+                    default:
+                        throw new RuntimeException("에러~");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("1 ~ 4 메뉴에 맞게 입력해주세요\n");
             }
         }
+
     }
 }
--- src/main/java/org/example/parser/StudentParser.java
+++ src/main/java/org/example/parser/StudentParser.java
@@ -3,13 +3,10 @@ package org.example.parser;
 import org.example.DBStorage;
 import org.example.domain.Student;
 
-import java.util.List;
-
 public class StudentParser {
 
     /**
-     * @찬원
-     * 수강생 정보 조회
+     * @찬원 수강생 정보 조회
      * throw 조회된 수강생 정보가 없을 경우
      */
     public void studentEmptyCheckValid(Integer studentIdInput) {
