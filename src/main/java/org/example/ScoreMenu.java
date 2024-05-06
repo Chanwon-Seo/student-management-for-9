861dff4df8f9829292a5e515ad72140d21d47933
haze0827 <167048997+haze0827@users.noreply.github.com>
2024-05-07 04:25:54 +0900

    UPDATE.StudentMenu.java.     1.특이사항.       - 수강생 등록, 수강생 조회 메뉴가 불안정하지만 활성화 되었습니다.
        2.StudentMenu,scoreMeun.java.
          - 숫자, 문자외 입력시"숫자외 문자를 입력하였습니다, 다시 입력바랍니다."를 "something wrong~, 다시 입력바랍니다."로 변경하였습니다.
    
          -사유: 메인메뉴 이동 후 각파트로 전환하여도 숫자외 문자등을 입력시 각각메뉴에 입력된 if-catch가 적용됨을 인지함.
            가.메인메뉴> 1. 수강생관리> 1. 수강생등록는 예외.
    
    ※메뉴 진입후 각파트로 연결하는건 continue... :") 끝.

--- src/main/java/org/example/ScoreMenu.java
+++ src/main/java/org/example/ScoreMenu.java
@@ -5,7 +5,9 @@ package org.example;
 
 import lombok.RequiredArgsConstructor;
 import org.example.parser.Parser;
+import org.example.parser.ScoreParser;
 import org.example.service.ScoreService;
+import org.example.service.StudentScoreRead;
 
 import static org.example.Menu.dbStorage;
 import static org.example.Menu.sc;
@@ -30,7 +32,6 @@ public class ScoreMenu {
                             System.out.println("***** 수강생 점수 등록*****");
                             System.out.println("메인메뉴> 수강생 점수관리>...");
 
-
                             new Parser().scoreCreate(1, 1, 1, 1);
                             dbStorage.saveScoreList(new ScoreService().scoreCreateV1(1, 1, 1, 1));
                             break;
@@ -40,14 +41,12 @@ public class ScoreMenu {
                             System.out.println("*****수강생 점수 조회*****");
                             System.out.println("메인메뉴> 수강생 점수관리>...");
 
-
-                            new Parser().scoreCreate(1, 1, 1, 1);
-                            dbStorage.saveScoreList(new ScoreService().scoreCreateV1(1, 1, 1, 1));
+                           // new StudentScoreRead().LoadScore();
                             break;
 
 
                         case 3:
-                            System.out.println("*****수강생 과목별 회차 점수 수정*****");
+                            System.out.println("*****수강생 과목별 회차점수 수정*****");
                             System.out.println("메인메뉴> 수강생 점수관리>...");
 
 
@@ -58,6 +57,7 @@ public class ScoreMenu {
                             System.out.println("이전 화면으로 돌아갑니다.");
                             System.out.println("메인메뉴> 이전메뉴로 이동>...");
                             System.out.printf("%n");
+
                             return;
 
 
@@ -66,7 +66,8 @@ public class ScoreMenu {
 
                     }
                 } catch (NumberFormatException e) {
-                    System.out.println("숫자외 문자를 입력하였습니다, 다시 입력바랍니다.");
+                    //System.out.println("숫자외 문자를 입력하였습니다, 다시 입력바랍니다.");
+                    System.out.println("something wrong~, 다시 입력바랍니다.");
                 }
             }
 //else {
--- src/main/java/org/example/StudentMenu.java
+++ src/main/java/org/example/StudentMenu.java
@@ -3,10 +3,15 @@
 
 package org.example;
 
+import org.example.domain.Student;
+import org.example.service.StudentService;
+
 import static org.example.Menu.dbStorage;
 import static org.example.Menu.sc;
 
 public class StudentMenu {
+
+
     public void displayStudentView() {
 
         while (true) {
@@ -15,38 +20,44 @@ public class StudentMenu {
             System.out.println("3. 이전메뉴로 이동");
             System.out.printf("%n");
 
-            int i = Integer.parseInt(sc.nextLine());
-            if (sc.hasNextLine()) ;
-            try {
-                switch (i) {
-                    case 1:
-                        System.out.println("***** 수강생 등록*****");
-                        System.out.println("메인메뉴> 수강생 관리>...");
+
+            if (sc.hasNextLine()) {
+                try {
+                    int i = Integer.parseInt(sc.nextLine());
+
+                    switch (i) {
+                        case 1:  //연결되었습니다.
+                            System.out.println("***** 수강생 등록*****");
+                            System.out.println("메인메뉴> 수강생 관리>...");
+                            
+                            new StudentService().createStudent();
+
+                            break;
+                        case 2: //연결되었습니다.
+                            System.out.println("***** 수강생 조회*****");
+                            System.out.println("메인메뉴> 수강생 관리>...");
+                            
+                           // new StudentService().getStudentDetail();
+                            new StudentService().getStudentList();
+
+                            break;
+
+                        case 3:
+                            System.out.println("이전 화면으로 돌아갑니다.");
+                            System.out.println("메인메뉴> 이전메뉴로 이동>...");
+                            
+                            return;
 
 
-                        break;
-                    case 2:
-                        System.out.println("***** 수강생 조회*****");
-                        System.out.println("메인메뉴> 수강생 관리>...");
-
-
-                        break;
-
-                    case 3:
-                        System.out.println("이전 화면으로 돌아갑니다.");
-                        System.out.println("메인메뉴> 이전메뉴로 이동>...");
-                        System.out.printf("%n");
-                        return;
-
-
-                    default:
-                        System.out.println("다시입력바랍니다.");
+                        default:
+                            System.out.println("다시입력바랍니다.");
+                    }
+                } catch (NumberFormatException e) {
+                    //ystem.out.println("숫자외 문자를 입력하였습니다, 다시 입력바랍니다.");
+                    System.out.println("something wrong~, 다시 입력바랍니다.");
                 }
-            } catch (NumberFormatException e) {
-                System.out.println("숫자외 문자를 입력하였습니다, 다시 입력바랍니다.");
-            }
-//else{
-//System.out.println("입력이 없습니다, 다시입력바랍니다.");
+            } else
+                System.out.println("입력이 없습니다, 다시입력바랍니다.");
         }
     }
 }
