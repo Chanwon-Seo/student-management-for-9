21730a8d65e6c2aa8f8dfa1b8b5bc2e5123f4397
dobam <ckehqja@gmail.com>
2024-05-09 16:29:24 +0900

    refactor: case 최신버전 수정, NumberFormatException 단계별로 터지게 해서 나가는 부분 조정

--- src/main/java/org/example/StudentMenu.java
+++ src/main/java/org/example/StudentMenu.java
@@ -42,14 +42,13 @@ public class StudentMenu {
                     int i = Integer.parseInt(sc.nextLine());
 
                     switch (i) {
-                        case 1:
+                        case 1 -> {
                             System.out.println("***** 수강생 등록*****");
                             System.out.println("메인메뉴> 수강생 관리>...");
 
                             studentService.createStudent();
-
-                            break;
-                        case 2:
+                        }
+                        case 2 -> {
                             System.out.println("***** 수강생 조회*****");
                             System.out.println("메인메뉴> 수강생 관리>...");
 
@@ -63,96 +62,99 @@ public class StudentMenu {
                                 System.out.println("3. 상태별 수강생 목록");
                                 System.out.println("4. 수강생 삭제");
                                 System.out.println("0. 뒤로가기");
-                                switch (Integer.parseInt(sc.nextLine())) {
-                                    case 1 -> {
-                                        System.out.println("조회할 수강생 아이디 입력");
-                                        studentId = Integer.parseInt(sc.nextLine());
-                                        try {
-                                            studentService.getStudentDetail(studentId);
-                                        } catch (Exception e) {
-                                            System.out.println(e.getMessage());
-                                        }
-                                    }
-                                    case 2 -> {
-                                        System.out.println("수정할 수강생의 아이디 입력");
-                                        studentId = Integer.parseInt(sc.nextLine());
-                                        Student findStudent = null;
-                                        try {
-                                            findStudent = studentService.studentFindById(studentId);
-                                        } catch (NullPointerException e) {
-                                            System.out.println(e.getMessage());
-                                        }
-                                        if (findStudent != null) {
-                                            System.out.println("수정할 정보를 입력(1.이름, 2.생일, 3.상태)>");
-                                            switch (Integer.parseInt(sc.nextLine())) {
-                                                case 1 -> {
-                                                    System.out.print("수정할 이름> ");
-                                                    String editName = sc.nextLine();
-                                                    studentService.editStudent(
-                                                            findStudent,
-                                                            editName,
-                                                            findStudent.getBirthDay(),
-                                                            findStudent.getStudentStateType());
-                                                }
-                                                case 2 -> {
-                                                    System.out.print("수정할 생일> ");
-                                                    String editBrithDay = sc.nextLine();
-                                                    studentService.editStudent(
-                                                            findStudent,
-                                                            findStudent.getStudentName(),
-                                                            editBrithDay,
-                                                            findStudent.getStudentStateType());
-                                                }
-                                                case 3 -> {
-                                                    //사용자가 입력받은 사용자의 상태
-                                                    studentStateType = getStudentStateType();
-                                                    //만약 값이 일치하는 것이 없으면 기존 사용자의 값을 가져온다.
-                                                    if (studentStateType == null) {
-                                                        studentStateType = findStudent.getStudentStateType();
-                                                        System.out.println("불일치 !! 기존값으로 ");
-                                                        System.out.println();
-                                                    }
-                                                    studentService.editStudent(
-                                                            findStudent,
-                                                            findStudent.getStudentName(),
-                                                            findStudent.getBirthDay(),
-                                                            studentStateType);
-                                                }
-                                                default -> System.out.println("불일치!!!");
+                                try {
+                                    switch (Integer.parseInt(sc.nextLine())) {
+                                        case 1 -> {
+                                            System.out.println("조회할 수강생 아이디 입력");
+                                            studentId = Integer.parseInt(sc.nextLine());
+                                            try {
+                                                studentService.getStudentDetail(studentId);
+                                            } catch (Exception e) {
+                                                System.out.println(e.getMessage());
                                             }
-                                        } else System.out.println();
+                                        }
+                                        case 2 -> {
+                                            System.out.println("수정할 수강생의 아이디 입력");
+                                            studentId = Integer.parseInt(sc.nextLine());
+                                            Student findStudent = null;
+                                            try {
+                                                findStudent = studentService.studentFindById(studentId);
+                                            } catch (NullPointerException e) {
+                                                System.out.println(e.getMessage());
+                                            }
+                                            if (findStudent != null) {
+                                                System.out.println("수정할 정보를 입력(1.이름, 2.생일, 3.상태)>");
+                                                switch (Integer.parseInt(sc.nextLine())) {
+                                                    case 1 -> {
+                                                        System.out.print("수정할 이름> ");
+                                                        String editName = sc.nextLine();
+                                                        studentService.editStudent(
+                                                                findStudent,
+                                                                editName,
+                                                                findStudent.getBirthDay(),
+                                                                findStudent.getStudentStateType());
+                                                    }
+                                                    case 2 -> {
+                                                        System.out.print("수정할 생일> ");
+                                                        String editBrithDay = sc.nextLine();
+                                                        studentService.editStudent(
+                                                                findStudent,
+                                                                findStudent.getStudentName(),
+                                                                editBrithDay,
+                                                                findStudent.getStudentStateType());
+                                                    }
+                                                    case 3 -> {
+                                                        //사용자가 입력받은 사용자의 상태
+                                                        studentStateType = getStudentStateType();
+                                                        //만약 값이 일치하는 것이 없으면 기존 사용자의 값을 가져온다.
+                                                        if (studentStateType == null) {
+                                                            studentStateType = findStudent.getStudentStateType();
+                                                            System.out.print("불일치!!!\n\n");
+                                                        }
+                                                        studentService.editStudent(
+                                                                findStudent,
+                                                                findStudent.getStudentName(),
+                                                                findStudent.getBirthDay(),
+                                                                studentStateType);
+                                                    }
+                                                    default -> System.out.print("불일치!!!\n\n");
+                                                }
+                                            } else System.out.println();
+                                        }
+                                        case 3 -> {
+                                            studentStateType = getStudentStateType();
+                                            studentService.studentListByStatus(studentStateType);
+                                        }
+                                        case 4 -> {
+                                            System.out.println("삭제할 수강생 아이디 입력");
+                                            studentId = Integer.parseInt(sc.nextLine());
+                                            studentService.deleteStudentById(studentId);
+                                            scoreService.deleteScoreByStudentId(studentId);
+                                        }
+                                        case 0 -> {
+                                            break loopA;
+                                        }
                                     }
-                                    case 3 -> {
-                                        studentStateType = getStudentStateType();
-                                        studentService.studentListByStatus(studentStateType);
-                                    }
-                                    case 4 -> {
-                                        System.out.println("삭제할 수강생 아이디 입력");
-                                        studentId = Integer.parseInt(sc.nextLine());
-                                        studentService.deleteStudentById(studentId);
-                                        scoreService.deleteScoreByStudentId(studentId);
-                                    }
-                                    case 0 -> {
-                                        break loopA;
-                                    }
+                                } catch (IllegalStateException e) {
+                                    System.out.println(e.getMessage());
+                                    System.out.println();
+
+                                } catch (NumberFormatException e2) {
+                                    System.out.print("일치하는 숫자만 입력하세요 !!\n\n");
                                 }
                             }
-
-                        case 0:
+                        }
+                        case 0 -> {
                             System.out.println("이전 화면으로 돌아갑니다.");
                             System.out.println("메인메뉴> 이전메뉴로 이동>...");
-
                             return;
-
-
-                        default:
-                            System.out.println("다시입력바랍니다.");
+                        }
+                        default -> System.out.println("다시입력바랍니다.");
                     }
                 } catch (NumberFormatException e) {
-                    System.out.println("something wrong~, 다시 입력바랍니다.");
+                    System.out.print("일치하는 숫자만 입력하세요 !!\n\n");
                 }
-            } else
-                System.out.println("입력이 없습니다, 다시입력바랍니다.");
+            }
         }
     }
 
@@ -166,7 +168,7 @@ public class StudentMenu {
             case "1" -> StudentStateType.GREEN;
             case "2" -> StudentStateType.RED;
             case "3" -> StudentStateType.YELLOW;
-            default -> null;
+            default -> throw new IllegalStateException("\n상태 불일치 항목!! \n\n");
         };
     }
 }
